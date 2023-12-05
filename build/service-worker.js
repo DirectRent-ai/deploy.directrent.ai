50887df991b045ecf61afe575f2726e6c94e026d
Liaqat Saeed <liaqatsaeed007@gmail.com>
2023-12-05 19:25:32 +0500

    build(version 3.2): map boundary and Dyanmic Zoom

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a75d269caacdcd7898bcb308b4e1b0c6.js"
+  "/precache-manifest.d89a34ef1ea3589c3b497951fde29df5.js"
 );
 
 self.addEventListener('message', (event) => {
